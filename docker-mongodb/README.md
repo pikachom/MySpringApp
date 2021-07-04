e09586342dcef58dc79887d04a600442407dec6b
pikachom <pikachom@gmail.com>
2021-07-05 04:19:20 +0900

    initial commit

---
+++ docker-mongodb/README.md
@@ -0,0 +1,5 @@
+# MongoDB run
+##Docker MongoDB run
+* $ docker-compose up -d
+* $ docker exec -it mongodb bash
+* (inside container) $ mongo -u admin
---
+++ src/main/java/com/spring/my/app/repository/TestsRepository.java
@@ -0,0 +1,10 @@
+//package com.spring.my.app.repository;
+//
+//import com.spring.my.app.entity.Test;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class TestsRepository extends MongoRepository<Test, String> {
+//
+//}
