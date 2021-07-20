0c709bface7eaaac64eae6ec012058cad8a27f3d
내 이름 <you@example.com>
2021-07-20 16:53:24 +0900

    충돌해결

--- src/main/java/com/spring/my/app/auth/config/CorsConfig.java
+++ src/main/java/com/spring/my/app/auth/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.spring.my.app.config;
+package com.spring.my.app.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
